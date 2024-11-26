32808654ecb5807b3f8f0e1327798c76655aa236
Jillian Fusco <fuscojillian@gmail.com>
2024-11-25 19:47:18 -0500

    Initial commit

---
+++ src/main/java/com/jillycodes/StudentSystem/controller/StudentController.java
@@ -0,0 +1,27 @@
+package com.jillycodes.StudentSystem.controller;
+
+import com.jillycodes.StudentSystem.model.Student;
+import com.jillycodes.StudentSystem.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/student")
+@CrossOrigin
+public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping("/add")
+    public String add(@RequestBody Student student) {
+        studentService.saveStudent(student);
+        return "New Student is added";
+    }
+
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
+    }
+}
---
+++ src/main/java/com/jillycodes/StudentSystem/service/StudentServiceImplementation.java
@@ -0,0 +1,25 @@
+package com.jillycodes.StudentSystem.service;
+
+import com.jillycodes.StudentSystem.model.Student;
+import com.jillycodes.StudentSystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImplementation implements StudentService{
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+}
